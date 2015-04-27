7da9615c4ef7d330f802c1ff63acbc0be64fcfbb
luis <luisbigvargas@gmail.com>
2015-04-27 00:20:37 -0400

    Adding tutorial 00 and 01

---
+++ MyFirstPolymerApp_01/lib/my_badge/my_badge.dart
@@ -0,0 +1,41 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer my-badge element.
+ */
+@CustomTag('my-badge')
+class MyBadge extends PolymerElement {
+
+  /// Constructor used to create instance of MyBadge.
+  MyBadge.created() : super.created() {
+  }
+  
+  @published String color = 'red';
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of my-badge is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of my-badge is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// my-badge is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when my-badge has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
